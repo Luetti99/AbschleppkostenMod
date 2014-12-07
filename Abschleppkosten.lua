176310a93f5fcd8e39cbbd383f5a1fe6fe31113f
Luetti99 <jens.luetti@gmail.com>
2014-12-07 14:29:48 +0100

    Create Abschleppkosten.lua

---
+++ Abschleppkosten.lua
@@ -0,0 +1,43 @@
+-- Abschleppkosten
+-- Author: Lütti
+-- fügt Abschleppkosten hinzu
+
+Abschleppkosten = {};
+
+function Abschleppkosten:loadMap(name)
+end;
+
+function Abschleppkosten:deleteMap()
+end;
+
+function Abschleppkosten:mouseEvent(posX, posY, isDown, isUp, button)
+end;
+
+function Abschleppkosten:keyEvent(unicode, sym, modifier, isDown)
+	if InputBinding.isPressed(InputBinding.moneyplus) then
+		g_currentMission.missionStats.money = g_currentMission.missionStats.money + 100000;
+		print("add 100.000 Euro")
+	end;
+	if InputBinding.isPressed(InputBinding.moneyminus) then
+		g_currentMission.missionStats.money = g_currentMission.missionStats.money - 100000;
+		print("delete 100.000 Euro")	
+    end;
+end;
+
+function Abschleppkosten: 	-- zurücksetz Event
+	if ... then
+		-- moddesc path von zurückgesetztem Objekt abfragen
+		Wert = getXMLInt(modDesc(modDesc.storeItems.storeItem.price))
+		AbKosten = Wert*0,02
+		g_currentMission.missionStats.money = g_currentMission.missionStats.money - AbKosten
+		print("Es wurden Abschleppkosten in Höhe von "..AbKosten.."€ abgezogen.")
+	end;	
+end;
+
+function Abschleppkosten:update(dt)
+end;
+
+function Abschleppkosten:draw()
+end;
+
+addModEventListener(Abschleppkosten);
