fb472207bcef40e3e01f297631821834f143490c
Luetti99 <jens.luetti@gmail.com>
2014-12-23 21:27:18 +0100

    Update Zusatzkosten.lua

--- Zusatzkosten.lua
+++ Zusatzkosten.lua
@@ -7,15 +7,8 @@ Freibetrag = 15000;
 print ("extendedFinaceModel loaded");
 
 function Zusatzkosten:loadMap(name)
+	g_currentMission.environment:addDayChangeListener(self); 
 	--print ("extendedFinaceModel loaded");
-	if g_currentMission.missionStats.difficulty == 1 then
-		multiplikator = 0.2;
-	elseif g_currentMission.missionStats.difficulty == 2 then
-		multiplikator = 0.12;
-	elseif g_currentMission.missionStats.difficulty == 3 then
-		multiplikator = 0.08;
-	end;
-	print("multiplikator = "..multiplikator);
 end;
 
 function Zusatzkosten:deleteMap()
@@ -29,31 +22,45 @@ function Zusatzkosten:keyEvent(unicode, sym, modifier, isDown)
 end;
 
 function Zusatzkosten:update(dt)
-	if dt == 24 then
-		Kost1 = g_currentMission.financeStats.newVehiclesCost;
-		Kost2 = g_currentMission.financeStats.newAnimalsCost;
-		Kost3 = g_currentMission.financeStats.constructionCost;
-		Kost4 = g_currentMission.financeStats.vehicleRunningCost;
-		Kost5 = g_currentMission.financeStats.propertyMaintenance;
-		Kost6 = g_currentMission.financeStats.wagePayment;
-		
-		Gewinn1 = g_currentMission.financeStats.harvestIncome;
-		Gewinn2 = g_currentMission.financeStats.missionIncome;
-		
-		Anderes1 = g_currentMission.financeStats.loanInterest;
-		Anderes2 = g_currentMission.financeStats.other;
-		
-		Gewinn = Kost1 + Kost2 + Kost3 + Kost4 + Kost5 + Kost6 + Gewinn1 + Gewinn2 + Anderes1 + Anderes2;
-		print ("Gewinn ="..Gewinn);
-		if Gewinn >= Freibetrag then
-			Steuern = (Gewinn - Freibetrag) * multiplikator;
-			g_currentMission.missionStats.money = g_currentMission.missionStats.money + Steuern;
-			print ("Es wurden Steuern in Hoehe von "..Steuern.." abgezogen.");
-		end;
-	end;
 end;
 
 function Zusatzkosten:draw()
 end;
+
+function Zusatzkosten:dayChanged()
+	local difficulty = g_currentMission.missionStats.difficulty;
+	print ("Schwierigkeit: "..difficulty);	
+	local multiplikator = 0.2/difficulty;
+	print("multiplikator: "..multiplikator);
+	
+	
+	-- local Kost1 = g_currentMission.economyManager.financeStats.newVehiclesCost;
+	-- print ("Kost1 ="..Kost1);
+	-- local Kost2 = g_currentMission.economyManager.financeStatsHistory.financeStats.newAnimalsCost;
+	-- local Kost3 = g_currentMission.economyManager.constructionCost;
+	-- local Kost4 = g_currentMission.economyManager.vehicleRunningCost;
+	-- local Kost5 = g_currentMission.economyManager.propertyMaintenance;
+	-- local Kost6 = g_currentMission.economyManager.wagePayment;
+	
+	-- local Gewinn1 = g_currentMission.economyManager.harvestIncome;
+	-- local Gewinn2 = g_currentMission.economyManager.missionIncome;
+	
+	-- local Anderes1 = g_currentMission.economyManager.loanInterest;
+	-- local Anderes2 = g_currentMission.economyManager.other;
+	
+	-- local Gewinn = Kost1 + Kost2 + Kost3 + Kost4 + Kost5 + Kost6 + Gewinn1 + Gewinn2 + Anderes1 + Anderes2;
+	
+	
+	local Finanzen = g_currentMission.economyManager.financeStatsHistory;
+
+	print (Finanzen[1].newAnimalsCost);
+	
+	--print ("Gewinn ="..Gewinn);
+	if Gewinn >= Freibetrag then
+		local Steuern = (Gewinn - Freibetrag) * multiplikator;
+		g_currentMission.missionStats.money = g_currentMission.missionStats.money + Steuern;
+		print ("Es wurden Steuern in Hoehe von "..Steuern.." abgezogen.");
+	end;
+end; 
 
 addModEventListener(Zusatzkosten);
