a0e64d8f6a1a7711746d78260cf085edbab77e0d
Luetti99 <jens.luetti@gmail.com>
2014-12-26 20:27:55 +0100

    Update Zusatzkosten.lua

--- Zusatzkosten.lua
+++ Zusatzkosten.lua
@@ -3,12 +3,11 @@
 -- fügt Zusatzkosten hinzu
 
 Zusatzkosten = {};
-Freibetrag = 15000;
-print ("extendedFinaceModel loaded");
+local Freibetrag = 15000;
 
 function Zusatzkosten:loadMap(name)
 	g_currentMission.environment:addDayChangeListener(self); 
-	--print ("extendedFinaceModel loaded");
+	print ("extendedFinaceModel(by Luetti) loaded");
 end;
 
 function Zusatzkosten:deleteMap()
@@ -28,38 +27,39 @@ function Zusatzkosten:draw()
 end;
 
 function Zusatzkosten:dayChanged()
-	local difficulty = g_currentMission.missionStats.difficulty;
-	print ("Schwierigkeit: "..difficulty);	
-	local multiplikator = 0.2/difficulty;
-	print("multiplikator: "..multiplikator);
+	if g_currentMission:getIsServer() then
+		local difficulty = g_currentMission.missionStats.difficulty;
+		print ("Schwierigkeit: "..difficulty);	
+		local multiplikator = 0.2/difficulty;
+		print("multiplikator: "..multiplikator);
 	
 	
-	-- local Kost1 = g_currentMission.economyManager.financeStats.newVehiclesCost;
-	-- print ("Kost1 ="..Kost1);
-	-- local Kost2 = g_currentMission.economyManager.financeStatsHistory.financeStats.newAnimalsCost;
-	-- local Kost3 = g_currentMission.economyManager.constructionCost;
-	-- local Kost4 = g_currentMission.economyManager.vehicleRunningCost;
-	-- local Kost5 = g_currentMission.economyManager.propertyMaintenance;
-	-- local Kost6 = g_currentMission.economyManager.wagePayment;
+		local Kost1 = g_currentMission.economyManager.financeStatsHistory.newVehiclesCost;
+		print ("Kost1 ="..Kost1);
+		local Kost2 = g_currentMission.economyManager.financeStatsHistory.newAnimalsCost;
+		local Kost3 = g_currentMission.economyManager.financeStatsHistory.constructionCost;
+		local Kost4 = g_currentMission.economyManager.financeStatsHistory.vehicleRunningCost;
+		local Kost5 = g_currentMission.economyManager.financeStatsHistory.propertyMaintenance;
+		local Kost6 = g_currentMission.economyManager.financeStatsHistory.wagePayment;
+		
+		local Gewinn1 = g_currentMission.economyManager.financeStatsHistory.harvestIncome;
+		local Gewinn2 = g_currentMission.economyManager.financeStatsHistory.missionIncome;
 	
-	-- local Gewinn1 = g_currentMission.economyManager.harvestIncome;
-	-- local Gewinn2 = g_currentMission.economyManager.missionIncome;
+		local Anderes1 = g_currentMission.economyManager.financeStatsHistory.loanInterest;
+		local Anderes2 = g_currentMission.economyManager.financeStatsHistory.other;
 	
-	-- local Anderes1 = g_currentMission.economyManager.loanInterest;
-	-- local Anderes2 = g_currentMission.economyManager.other;
+		local Gewinn = Kost1 + Kost2 + Kost3 + Kost4 + Kost5 + Kost6 + Gewinn1 + Gewinn2 + Anderes1 + Anderes2;
+		
+		-- local moneyold = g_currentMission:getpreviousDayMoney();
+		-- local money = g_currentMission:getMoney();
+		-- local Gewinn = money - moneyold
 	
-	-- local Gewinn = Kost1 + Kost2 + Kost3 + Kost4 + Kost5 + Kost6 + Gewinn1 + Gewinn2 + Anderes1 + Anderes2;
-	
-	
-	local Finanzen = g_currentMission.economyManager.financeStatsHistory;
-
-	print (Finanzen[1].newAnimalsCost);
-	
-	--print ("Gewinn ="..Gewinn);
-	if Gewinn >= Freibetrag then
-		local Steuern = (Gewinn - Freibetrag) * multiplikator;
-		g_currentMission.missionStats.money = g_currentMission.missionStats.money + Steuern;
-		print ("Es wurden Steuern in Hoehe von "..Steuern.." abgezogen.");
+		print ("Gewinn ="..Gewinn);
+		if Gewinn >= Freibetrag then
+			local Steuern = (Gewinn - Freibetrag) * multiplikator;
+			g_currentMission:addMoney(Steuern, "other")
+			print ("Es wurden Steuern in Hoehe von "..Steuern.." abgezogen.");
+		end;
 	end;
 end; 
 
